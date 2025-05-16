633d220d3ec03e1b30d19377ddba065e073b1511
SavioFagundes <AkiraSfM@gmail.com>
2025-05-15 22:34:56 -0300

    feat:Estruturando projeto

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,6 +1,16 @@
+using minimal_api.dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/login", (loginDTO loginDTO) => {
+    if(loginDTO.Email == "adm@teste.com" && loginDTO.senha == "123456")
+        return Results.Ok("Login realizado com sucesso");
+    else
+        return Results.BadRequest("Email ou senha inválidos");
+});
+
 app.Run();
+
